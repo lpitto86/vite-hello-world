c1bd6bcf1ead6d454a7a74f4b296a94c8bd5ea9d
lpitto86 <luca.p481@gmail.com>
2024-01-22 17:00:00 +0100

    AppVue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <script>
+/* Importazione del componente */
+
+export default {
+  data() {
+    return { 
+      count: 0 
+    }
+  },
+  methods: {
+    incrementCount () {
+      this.count++
+    }
+  }
+}
 </script>
 
 <template>
+  <div>
+    <h1>
+      {{ count }}
+    </h1>
+    <button @click="incrementCount()">
+      Incrementa counter
+    </button>
+  </div>
 </template>
 
 <style scoped>
+h1 {
+  color: lightcoral;
+}
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {  
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      Vite Hello
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  color: lightcoral;
+}
+</style>
